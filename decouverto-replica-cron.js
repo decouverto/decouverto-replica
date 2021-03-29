3c0235e9f8017ec7b865c39e2b6ca933507f481f
Cédric JUNG <cedced19@gmail.com>
2021-03-29 22:08:03 +0200

    Change CRON job

--- decouverto-replica-cron.js
+++ decouverto-replica-cron.js
@@ -1,6 +1,6 @@
 const schedule = require('node-schedule');
 const exec = require('child_process').exec;
 
-schedule.scheduleJob('0 0 0 * * 0', function() {
+schedule.scheduleJob('0 0 12 ? * TUE *', function() {
     exec('node decouverto-replica.js', console.log);
 });
